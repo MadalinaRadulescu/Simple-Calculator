1071e3823318ed4d0281fd34272e0bdbefd935d3
MadalinaRadulescu <radulescumadalina90@gmail.com>
2022-04-10 16:07:04 +0300

    Done

--- calculator.py
+++ calculator.py
@@ -1,30 +1,92 @@
 def is_number(str):
-    pass
-
+    if str.isnumeric() is True:
+        return True
+    elif str.lstrip("-").isnumeric() is True:
+        return True
+    try:
+        float(str)
+        return True
+    except ValueError:
+        return False
 
 def convert_number(str):
-    pass
-
+    if is_number(str) is True:
+        try:
+            str = int(str)
+            return str
+        except ValueError:
+            return str
+    
 
 def is_valid_operator(operator):
-    pass
+    if operator == "+":
+        return True
+    elif operator == "-":
+        return True
+    elif operator == "*":
+        return True
+    elif operator == "/":
+        return True
+    else:
+        return False
 
 
 def ask_for_a_number(force_valid_input):
-    pass
-
+    while True:
+        num = input("Please provide a number! ")
+        if is_number(num) is True:
+            return convert_number(num)
+        else:
+            if not force_valid_input:
+                return None
+            print("This didn't look like a number, try again.")
+            
+            
+            
 
 def ask_for_an_operator(force_valid_input):
-    pass
+    while True:
+        oper = input("Please provide an operator (one of +, -, *, /)!")
+        if is_valid_operator(oper) is True:
+            return oper
+        else:
+            if not force_valid_input:
+                return None
+            print("Unknown operator.")
 
 
 def calc(operator, a, b):
-    pass
+    if not is_valid_operator(operator):
+        return None
+    result = None
+    if operator == "+":
+        result = a + b
+        return result
+    elif operator == "-":
+        result = a - b
+        return result
+    elif operator == "*":
+        result = a * b
+        return result
+    elif operator == "/":
+        if b == 0:
+            print("Error: Division by zero")
+            return None
+        else:
+            result = a / b
+            return result
 
 
 def simple_calculator():
-    pass
-
+    while True:
+        a = ask_for_a_number(force_valid_input=False)
+        if a is None:
+            break
+        op = ask_for_an_operator(force_valid_input=True)
+        b = ask_for_a_number(force_valid_input=True)
+        result = calc(op, a, b)
+        if result is not None:
+            print(f"The result is {result}")
 
 if __name__ == '__main__':
     simple_calculator()
